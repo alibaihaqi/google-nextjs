2fc342c979818069ba3a2239b478c9ffdd8d7cef
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-26 23:55:41 +0700

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  images: {
+    // Use assets from Wikimedia
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '**',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
