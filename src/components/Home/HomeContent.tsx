d7c7422c9ba52498e5f8a5a3744fca9285c809ad
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-28 01:53:22 +0700

    feat: update home content

--- src/components/Home/HomeContent.tsx
+++ src/components/Home/HomeContent.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
+
 import HomeSearch from '@/components/Home/HomeSearch'
+import { GOOGLE_LOGO } from '@/constants/logo'
 
 export default function HomeContent() {
   return (
-    <section className='flex flex-1 flex-col items-center mt-32'>
+    <section className='flex flex-1 flex-col items-center mt-32 pb-32'>
       <Image
-        alt={'Google Logo'}
-        src={'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png'}
+        alt={GOOGLE_LOGO.alt}
+        src={GOOGLE_LOGO.url}
         width={272}
         height={92}
         priority
