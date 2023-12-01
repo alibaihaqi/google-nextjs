f6dc33edacc57c87de23f2836a169eb4149b63d1
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-01 15:50:19 +0700

    feat: add service custom search google

---
+++ src/services/google/search.ts
@@ -0,0 +1,10 @@
+export const GoogleCustomSearchApi = async (query: string): Promise<any> => {
+  if (!query) return
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${query}`
+  )
+  
+  if (!response.ok) new Error('Error query search')
+
+  return response.json()
+}
