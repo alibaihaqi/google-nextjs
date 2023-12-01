e2f19e4f70d6d9df6cfdfbf1fd5f1f356cd14d06
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-01 15:49:27 +0700

    feat: update web search about and query data

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,7 +1,12 @@
-export default async function SearchWebPage() {
+import WebSearchAbout from '@/components/WebSearch/WebSearchAbout'
+import { GoogleCustomSearchApi } from '@/services/google/search'
+
+export default async function SearchWebPage(ctx: any) {
+  const getSearchData = await GoogleCustomSearchApi(ctx.searchParams?.searchTerm || '')
+
   return (
-    <main className='flex flex-1 flex-col'>
-      SearchWebPage
+    <main className='flex flex-1 flex-col px-6'>
+      <WebSearchAbout searchInfo={getSearchData.searchInformation} />
     </main>
   )
 }
