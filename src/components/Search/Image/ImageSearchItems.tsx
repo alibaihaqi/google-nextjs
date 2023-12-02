73303330598cdb6b3678ade0ff268421a9cf0b5b
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-03 02:07:41 +0700

    feat: update styling image search items

--- src/components/Search/Image/ImageSearchItems.tsx
+++ src/components/Search/Image/ImageSearchItems.tsx
@@ -13,40 +13,40 @@ export default function ImageSearchItems({ items }: ISearchItemsProps<IImageSear
       return (
         <div
           key={idx}
-          className=''
+          className='group'
         >
-          <div className='group'>
-            <Link href={item.image.contextLink}>
-              <img
-                alt={item.title}
-                className='h-80 sm:h-60 group-hover:shadow-xl w-full object-contain transition-shadow'
-                src={item.link}
-              />
-            </Link>
-
-            <Link href={item.image.contextLink}>
-              <h2 className="group-hover:underline truncate text-lg pt-2 m-0">
-                { item.title }
-              </h2>
-            </Link>
-
-            <Link href={item.image.contextLink}>
-              <p className="group-hover:underline text-sm text-gray-600">
-                {item.displayLink}
-              </p>
-            </Link>
-          </div>
+          <Link href={item.image.contextLink}>
+            <img
+              alt={item.title}
+              className={`
+                h-40 sm:h-60 group-hover:shadow-xl
+                object-contain transition-shadow
+                self-center
+              `} 
+              src={item.link}
+            />
+          </Link>
+          <Link href={item.image.contextLink}>
+            <h2 className="group-hover:underline sm:truncate text-lg pt-2 m-0">
+              { item.title }
+            </h2>
+          </Link>
+          <Link href={item.image.contextLink}>
+            <p className="group-hover:underline text-sm text-gray-600">
+              {item.displayLink}
+            </p>
+          </Link>
         </div>
       )
     })
   }
 
   return (
-    <div className='sm:pb-24 pb-40  mt-8'>
+    <div className='pb-24 mt-8'>
       <section
         className={`
           grid grid-cols sm:grid-cols-3 lg:grid-cols-4
-          xl:grid-cols-5 space-x-4 px-3 sm:gap-x-4 gap-y-8
+          xl:grid-cols-5 sm:space-x-4 px-3 sm:gap-x-4 gap-y-8
         `}
       >
         { renderSearchItems() }
