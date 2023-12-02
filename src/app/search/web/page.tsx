638b9cad8429e5a50ef9cf08b694a97f246f551c
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-02 18:28:33 +0700

    feat: rename import component

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,13 +1,15 @@
-import WebSearchAbout from '@/components/WebSearch/WebSearchAbout'
-import WebSearchItems from '@/components/WebSearch/WebSearchItems'
+import SearchAbout from '@/components/Search/Common/SearchAbout'
+import WebSearchItems from '@/components/Search/Web/WebSearchItems'
 import { GoogleCustomSearchApi } from '@/services/google/search'
 
 export default async function SearchWebPage(ctx: any) {
-  const getSearchData = await GoogleCustomSearchApi(ctx.searchParams?.searchTerm || '')
+  const getSearchData = await GoogleCustomSearchApi({
+    query: ctx.searchParams?.searchTerm || '',
+  })
 
   return (
     <main className='flex flex-1 flex-col px-8'>
-      <WebSearchAbout searchInfo={getSearchData?.searchInformation} />
+      <SearchAbout searchInfo={getSearchData?.searchInformation} />
 
       <WebSearchItems items={getSearchData.items} />
     </main>
