4bbcebba0e05198aee7b403729519ed2e8d943f4
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-02 18:29:42 +0700

    feat: update search image page

--- src/app/search/image/page.tsx
+++ src/app/search/image/page.tsx
@@ -1,7 +1,18 @@
-export default async function SearchImagePage() {
+import SearchAbout from '@/components/Search/Common/SearchAbout'
+import ImageSearchItems from '@/components/Search/Image/ImageSearchItems'
+import { GoogleCustomSearchApi } from '@/services/google/search'
+
+export default async function SearchImagePage(ctx: any) {
+  const getSearchData = await GoogleCustomSearchApi({
+    query: ctx.searchParams?.searchTerm || '',
+    isSearchImage: true,
+  })
+
   return (
-    <main className='flex flex-1 flex-col'>
-      SearchImagePage
+    <main className='flex flex-1 flex-col px-8'>
+      <SearchAbout searchInfo={getSearchData?.searchInformation} />
+
+      <ImageSearchItems items={getSearchData?.items} />
     </main>
   )
 }
