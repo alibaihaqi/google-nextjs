7f78407ac40f0d0c9fe4cfd2ae3ad6d3bd9c28c5
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-03 15:46:42 +0700

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import TanstackProvider from '@/providers/TanStackProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,7 +26,9 @@ export default function RootLayout({
       </head>
 
       <body className={inter.className}>
-        { children }
+        <TanstackProvider>
+          { children }
+        </TanstackProvider>
       </body>
     </html>
   )
