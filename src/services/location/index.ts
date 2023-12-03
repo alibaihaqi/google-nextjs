9c1f35b38e0208ecbb441dc4845d049dfbee95ca
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-03 15:46:11 +0700

    feat: update location service

--- src/services/location/index.ts
+++ src/services/location/index.ts
@@ -1,15 +1,34 @@
-export const getIpData = async () => {
-  const res = await fetch(`${process.env.APIIP_PROTOCOL}://apiip.net/api/check?accessKey=${process.env.APIIP_KEY}`)
+export const getGeolocationIp = async () => {
+  const API_URL = process.env.NEXT_PUBLIC_API_IPIFY_URL as string;
+
+  const res = await fetch(API_URL);
 
   if (!res.ok) {
     return {
       success: false,
-      errorMessage: 'Failed to fetch API IP data',
+      errorMessage: 'Failed to fetch API Geolocation data',
     }
   }
 
   return {
     success: true,
-    location: await res.json()
+    clientData: await res.json()
   }
 }
+
+export const GetClientCountryApi = async () => {
+  const getGeolocationData = await getGeolocationIp()
+
+  if (!getGeolocationData.success) return getGeolocationData
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APIGW_DOMAIN}/apiip/check?ip=${getGeolocationData.clientData?.ip}`);
+
+  if (!res.ok) {
+    return {
+      success: false,
+      errorMessage: 'Failed to fetch API GetClientCountryApi data',
+    }
+  }
+
+  return res.json()
+} 
